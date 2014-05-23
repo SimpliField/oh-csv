0279cfa2bc78b3ed49cce50c7462397cdabe9f43
Nicolas Froidure <nicolas_froidure@elitwork.com>
2014-05-23 15:51:12 +0200

    Adding escaped content tests

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 var Stream = require('readable-stream');
 var util = require('util');
+var escapeRegExpComponent = require('escape-regexp-component');
 
 // Predefined configuration
 var csvOpts = {
   sep: [','],
   esc: ['\\'],
-  quotes: ['"'],
-  linesep: ['\n', '\r', '\r\n'],
+  quotes: [''],
+  linesep: ['\r\n', '\n', '\r'],
   charsEncoding: 'ascii'
 };
 var tsvOpts = {
   sep: ['\t'],
   esc: ['\\'],
-  quotes: ['"'],
-  linesep: ['\n', '\r', '\r\n'],
+  quotes: [''],
+  linesep: ['\r\n', '\n', '\r'],
   charsEncoding: 'ascii'
 };
 
@@ -27,11 +28,39 @@ var csv = {
 
 // Options integrity
 function checkOptions(options) {
-  options.sep = options.sep || csvOpts.sep;
-  options.esc = options.esc || '' === options.esc ? options.esc : csvOpts.esc;
-  options.quotes = options.quotes || '' === options.quotes ? options.quotes : csvOpts.quotes;
-  options.linesep = options.linesep || csvOpts.linesep;
+  // Required
+  if(options.sep && 'string' === typeof options.sep) {
+    options.sep = [options.sep];
+  } else {
+    options.sep = options.sep || csvOpts.sep;
+  }
+  if(!options.sep.length) {
+    throw Error('The option.sep argument is required.')
+  }
+  if(options.linesep && 'string' === typeof options.linesep) {
+    options.linesep = [options.linesep];
+  } else {
+    options.linesep = options.linesep || csvOpts.linesep;
+  }
+  if(!options.linesep.length) {
+    throw Error('The option.sep argument is required.')
+  }
   options.charsEncoding = options.charsEncoding || csvOpts.charsEncoding;
+  // Optionnal
+  if('string' === typeof options.esc) {
+    options.esc = [options.esc];
+  } else {
+    options.esc = options.esc || csvOpts.esc;
+  }
+  if(options.quotes && 'string' === typeof options.quotes) {
+    options.quotes = [options.quotes];
+  } else {
+    options.quotes = options.quotes || [];
+  }
+  options.charsToEscape = (
+    options.charsToEscape ||
+    options.quotes.concat(options.sep).concat(options.linesep)
+  ).filter(function(s) { return s; });
   return options;
 }
 
@@ -81,6 +110,13 @@ function CSVEncoder(options) {
   // Keep the options
   this.options = checkOptions(options);
 
+  // Build the escape search regular expression
+  this._escRegExp = new RegExp(
+    '(' + this.options.charsToEscape.map(escapeRegExpComponent).join('|') + ')',
+    'gm'
+  );
+  console.log(this._escRegExp);
+
 }
 
 // Inherit of transform stream
@@ -89,26 +125,35 @@ util.inherits(CSVEncoder, Stream.Transform);
 // Implement _transform underlying function
 CSVEncoder.prototype._transform = function csvEncoderTransform(row, encoding, cb) {
   var chunk;
+  var _self = this;
   // Flattening objects
   if(!(row instanceof Array)) {
-    if(!(this.options.fields && this.options.fields.length)) {
-      throw new Error('Cannot flat the given object since no field were defined.');
+    if(!(_self.options.fields && _self.options.fields.length)) {
+      throw new Error('Cannot flat the given object since no fields were defined.');
     }
-    row = this.options.fields.map(function(field){
+    row = _self.options.fields.map(function(field){
       return row[field];
     });
   }
   // Creating the chunk
   if(null !== encoding) {
-    this.push(new Buffer(row.join(this.options.sep[0]) + this.options.linesep[0], encoding));
+    _self.push(new Buffer(
+      row.map(function(field) {
+        return (field+'').replace(
+          _self._escRegExp,
+          _self.options.esc[0] + '$1'
+        );
+      }).join(_self.options.sep[0]) + _self.options.linesep[0],
+      encoding
+    ));
   } else {
     row = row.reduce(function(buffers, buf) {
       if(buffers.length) {
-        buffers.push(new Buffer(path.sep[0], this.optionscharsEncoding));
+        buffers.push(new Buffer(path.sep[0], _self.optionscharsEncoding));
       }
       buffers.push(buf);
     }, []);
-    this.push(Buffer.concat(row, row.length));
+    _self.push(Buffer.concat(row, row.length));
   }
   cb();
 };
--- tests/index.mocha.js
+++ tests/index.mocha.js
@@ -31,40 +31,85 @@ describe('csv encoder', function() {
 
   describe('with arrays', function() {
 
-    it('should work with simple input and csv config', function(done) {
-      var encoder = new csv.Encoder(csv.csvOpts);
-      getStreamText(encoder, function(text) {
-        assert.equal(text,
-          '1,test1,another test1\n' +
-          '2,test2,another test2\n' +
-          '3,test3,another test3\n' +
-          '4,test4,another test4\n'
-        );
-        done();
-      })
-      encoder.write([1, 'test1', 'another test1']);
-      encoder.write([2, 'test2', 'another test2']);
-      encoder.write([3, 'test3', 'another test3']);
-      encoder.write([4, 'test4', 'another test4']);
-      encoder.end();
+    describe('should work with csv config', function() {
+
+      it('and simple input', function(done) {
+        var encoder = new csv.Encoder(csv.csvOpts);
+        getStreamText(encoder, function(text) {
+          assert.equal(text,
+            '1,test1,another test1\r\n' +
+            '2,test2,another test2\r\n' +
+            '3,test3,another test3\r\n' +
+            '4,test4,another test4\r\n'
+          );
+          done();
+        });
+        encoder.write([1, 'test1', 'another test1']);
+        encoder.write([2, 'test2', 'another test2']);
+        encoder.write([3, 'test3', 'another test3']);
+        encoder.write([4, 'test4', 'another test4']);
+        encoder.end();
+      });
+
+      it('and input needing escape', function(done) {
+        var encoder = new csv.Encoder(csv.csvOpts);
+        getStreamText(encoder, function(text) {
+          assert.equal(text,
+            '1,te\\,st1,ano\\,ther \\,test1\r\n' +
+            '2,te\\,st2,ano\\,ther \\,test2\r\n' +
+            '3,te\\,st3,ano\\,ther \\,test3\r\n' +
+            '4,te\\,st4,ano\\,ther \\,test4\r\n'
+          );
+          done();
+        });
+        encoder.write([1, 'te,st1', 'ano,ther ,test1']);
+        encoder.write([2, 'te,st2', 'ano,ther ,test2']);
+        encoder.write([3, 'te,st3', 'ano,ther ,test3']);
+        encoder.write([4, 'te,st4', 'ano,ther ,test4']);
+        encoder.end();
+      });
+
     });
 
-    it('should work with simple input and tsv config', function(done) {
-      var encoder = new csv.Encoder(csv.tsvOpts);
-      getStreamText(encoder, function(text) {
-        assert.equal(text,
-          '1\ttest1\tanother test1\n' +
-          '2\ttest2\tanother test2\n' +
-          '3\ttest3\tanother test3\n' +
-          '4\ttest4\tanother test4\n'
-        );
-        done();
-      })
-      encoder.write([1, 'test1', 'another test1']);
-      encoder.write([2, 'test2', 'another test2']);
-      encoder.write([3, 'test3', 'another test3']);
-      encoder.write([4, 'test4', 'another test4']);
-      encoder.end();
+    describe('should work with tsv config', function() {
+
+      it('and simple input', function(done) {
+
+        var encoder = new csv.Encoder(csv.tsvOpts);
+        getStreamText(encoder, function(text) {
+          assert.equal(text,
+            '1\ttest1\tanother test1\r\n' +
+            '2\ttest2\tanother test2\r\n' +
+            '3\ttest3\tanother test3\r\n' +
+            '4\ttest4\tanother test4\r\n'
+          );
+          done();
+        });
+        encoder.write([1, 'test1', 'another test1']);
+        encoder.write([2, 'test2', 'another test2']);
+        encoder.write([3, 'test3', 'another test3']);
+        encoder.write([4, 'test4', 'another test4']);
+        encoder.end();
+      });
+
+      it('and input needing escape', function(done) {
+        var encoder = new csv.Encoder(csv.tsvOpts);
+        getStreamText(encoder, function(text) {
+          assert.equal(text,
+            '1\tte\\\tst1\\\n\t\\\r\nano\\\tther\\\r \\\ttest1\r\n' +
+            '2\tte\\\tst2\\\n\t\\\r\nano\\\tther\\\r \\\ttest2\r\n' +
+            '3\tte\\\tst3\\\n\t\\\r\nano\\\tther\\\r \\\ttest3\r\n' +
+            '4\tte\\\tst4\\\n\t\\\r\nano\\\tther\\\r \\\ttest4\r\n'
+          );
+          done();
+        });
+        encoder.write([1, 'te\tst1\n', '\r\nano\tther\r \ttest1']);
+        encoder.write([2, 'te\tst2\n', '\r\nano\tther\r \ttest2']);
+        encoder.write([3, 'te\tst3\n', '\r\nano\tther\r \ttest3']);
+        encoder.write([4, 'te\tst4\n', '\r\nano\tther\r \ttest4']);
+        encoder.end();
+      });
+
     });
 
   });
